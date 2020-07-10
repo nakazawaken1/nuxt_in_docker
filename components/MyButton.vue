10f1adaaac2e98f223b2e2f8928f3a0d1adc3f5e
nakazawaken1 <nakazawaken1@gmail.com>
2020-07-10 11:41:46 +0000

    スタイルを適用

--- components/MyButton.vue
+++ components/MyButton.vue
@@ -1,3 +1,15 @@
 <template>
-  <a>ボタン</a>
+  <a class="MyButton">ボタン</a>
 </template>
+
+<style>
+.MyButton {
+  background-color: #00a656;
+  border-radius: 1.5em;
+  box-shadow: 0 0.2em 0.5em rgba(0, 0, 0, 0.2);
+  padding: 1em 2em;
+  color: #ffffff;
+  font-weight: bold;
+  text-decoration: none;
+}
+</style>
